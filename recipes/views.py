014ce0bc0efcb9c042fdbd3e9468d06073f1d9de
wendellast <wendelalves01a@hotmail.com>
2023-05-30 09:01:20 -0300

    finalish

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html', context={
+    return render(request, 'home.html', context={
         'name': 'wendellast'
     }
     )
