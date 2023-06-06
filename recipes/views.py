007f2d3773b0ef113c7bf7755814f732cc5b4aca
wendellast <wendelalves01a@hotmail.com>
2023-06-06 09:06:56 -0300

    add partils

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'wendellast'
     }
     )
