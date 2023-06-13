986d8765a2dc6e54c807d937ba3fdf1cf900982b
wendellast <wendelalves02a@hotmail.com>
2023-06-13 17:30:49 -0300

    add static + block

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
+
+# from recipes.views import home
+
 
 urlpatterns = [
-    path('', home)
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipes)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -6,5 +6,12 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'wendellast'
-    }
-    )
+            }
+)
+    
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-views.html', context={
+        'name': 'wendellast'
+            }
+)
+    
