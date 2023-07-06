490e47a18d5b30c2ee4d8341226e5029598bd3eb
wendellast <wendelalves02a@hotmail.com>
2023-07-06 07:52:13 -0300

    add view category

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-# Create your views here.
+from .models import Recipe
 
 
 def home(request):
+    recipe = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipe,
     })
     
+def category(request, category_id):
+    recipe = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipe,
+    })
     
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-views.html', context={
