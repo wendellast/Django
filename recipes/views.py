e9cee897be00d979f962de04b386630dfa4bfc8d
wendellast <wendelalves02a@hotmail.com>
2023-07-06 08:09:24 -0300

    change view

--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,14 @@ from .models import Recipe
 
 
 def home(request):
-    recipe = Recipe.objects.all().order_by('-id')
+    recipe = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipe,
     })
     
 def category(request, category_id):
-    recipe = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipe = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipe,
     })
     
